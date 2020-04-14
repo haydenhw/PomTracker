e53d49964e2ab010fc8bc93de6e8827f1cb26c17
hayden7913 <hayden321@gmail.com>
2020-04-13 20:23:26 -0700

    Adds data file

---
+++ client/data.js
@@ -0,0 +1,26 @@
+const projects =  [{
+  project_name: 'Learn django',
+  id: 3,
+  user_id: 2,
+  client_id: '0n3lsMkiC6',
+  date_created: '2020-04-14T10:06:06.024Z',
+  tasks:
+    [{
+      task_name: 'Read docs',
+      recorded_time: 0,
+      id: 11,
+      client_id: 'ET4fdBZdNt',
+      user_id: 2,
+      project_id: 3,
+      date_created: '2020-04-14T10:06:06.216Z'
+    },
+      {
+        task_name: 'Build todo app',
+        recorded_time: 0,
+        id: 12,
+        client_id: 'avlm5LO8VY',
+        user_id: 2,
+        project_id: 3,
+        date_created: '2020-04-14T10:06:06.216Z'
+      }]
+}]
